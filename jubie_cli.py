f20bc68c32f25ccec5efef6e4105c5571ec900f0
Hector Flores <iitsumi1999@gmail.com>
2026-03-06 19:55:56 -0600

    Add Tux ASCII art to jubie_cli.py
    Added ASCII art representation of Tux, the Linux mascot.

--- jubie_cli.py
+++ jubie_cli.py
@@ -108,14 +108,45 @@ EASTER_EGG_TEMPEL_URL = "https://youtu.be/hmjU-6tkEc8?si=0QV_sQTMb6TUmoy2"
 EASTER_EGG_YOUNG_URL = "https://youtu.be/1KeCfeDagCE?si=b8V5S6203OuXMBTC"
 
 TUX_ASCII_ART = r"""
-        .---.
-       /     \
-       |. _ .|
-       ||   ||  ___
-       |'._.'| (   )
-       |     |  `-'
-       '-----'
-
+       
+                         4MMMMMMMMMMMML
+                       4MMMMMMMMMMMMMMMML
+                      MMMMMMMMMMMMMMMMMMML
+                     4MMMMMMMMMMMMMMMMMMMMM
+                    4MMMMMMMMMMMMMMMMMMMMMML
+                    MMMMP   MMMMMM   MMMMMMM
+                    MMMM MM  MMM  MM  MMMMMM
+                    MMMM MM  MMM  MM  MMMMML
+                     MMM MP,,,,,,,MM  MMMMMM
+                      MM,"          "MMMMMMP
+                      MMw           'MMMMMM
+                      MM"w         w MMMMMMML
+                      MM" w       w " MMMoMMML
+                     MMM " wwwwwww "  MMMMMMML
+                   MMMP   ".,,,,,,"     MMMMMMMML
+                  MMMP                    MMMMMMMML
+                MMMMM                      MMMMMMMML
+               MMMMM,,-''             ''-,,MMMMMMMMML
+              MMMMM                          MMMMMMMMML
+             MMMMM                            MMMMMMMMML
+            MMMMM                             MMMMMMMMMM
+            MMMM                               MMMMMMMMMM
+           MMMMM                               MMMMMMMMMML
+          MMMMM                                MMMMMMMMMMM
+         MMMMMM                                MMMMMMMMMMM
+         MMMMMMM                               MMMMMMMMMMM
+         """"MMMM                             MMMMMMMMMMP
+        "     ""MMM                            MMMMMMMMP
+   "" "         "MMMMMM                      """"MMMMMP"""
+ "               "MMMMMMM                   ""   """"""   "
+ "                ""MMMMMM                 M"             " ""
+  "                 "                   MMM"                  "
+ "                   "M               MMMM"                   "
+ "                    "MM        MMMMMMMMM"                ""
+ "                    "MMMMMMMMMMMMMMMMMMM"              """
+  """"                "MMMMMMMMMMMMMMMMMM"           """"
+      """"""""       MMMMM               "        ""
+              """"""""                      """"""" 
     Tux - the Linux mascot
 """
 
